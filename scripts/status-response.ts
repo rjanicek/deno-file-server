cc3cdbfc2885f6e38dc2baa4ba7e1e3f9778bafb
Richard Janicek <r@janicek.co>
2024-07-28 20:31:50 -0400

    * fixed status page title

--- scripts/status-response.ts
+++ scripts/status-response.ts
@@ -27,7 +27,7 @@ export function handleStatusResponse(request: Request) {
     <meta charset="utf-8">
     <meta http-equiv="refresh" content="${denoConfig["status-refresh-seconds"]}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Status - Deno File Server</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
